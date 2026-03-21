438752fae2c9c80aa9542edab590140616a576e8
Kareem Hosny <hsnykrym27@gmail.com>
2026-03-21 20:54:09 +0200

    Create saveResults.js

---
+++ netlify/functions/saveResults.js
@@ -0,0 +1,75 @@
+exports.handler = async function(event) {
+
+try {
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+
+const repo = "kareemretetert/third-year-secondary-website"
+const path = "results.json"
+
+const data = JSON.parse(event.body)
+
+// هات الملف الحالي
+const fileRes = await fetch(
+`https://api.github.com/repos/${repo}/contents/${path}`,
+{
+headers: {
+Authorization: `token ${GITHUB_TOKEN}`
+}
+}
+)
+
+let sha = null
+
+if (fileRes.status === 200) {
+const file = await fileRes.json()
+sha = file.sha
+}
+
+// تجهيز البيانات
+const updatedContent = Buffer
+.from(JSON.stringify(data, null, 2))
+.toString("base64")
+
+// رفع التعديل
+const updateRes = await fetch(
+`https://api.github.com/repos/${repo}/contents/${path}`,
+{
+method: "PUT",
+headers: {
+Authorization: `token ${GITHUB_TOKEN}`,
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+message: "update summaries",
+content: updatedContent,
+sha: sha || undefined,
+branch: "main"
+})
+}
+)
+
+const result = await updateRes.json()
+
+if (updateRes.status >= 400) {
+return {
+statusCode: 500,
+body: JSON.stringify(result)
+}
+}
+
+return {
+statusCode: 200,
+body: "Summaries updated successfully"
+}
+
+} catch (err) {
+
+return {
+statusCode: 500,
+body: err.message
+}
+
+}
+
+}
